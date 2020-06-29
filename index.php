e1adfa59c1d0b0b4503b42e90c595b8df0230af3
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-29 16:58:47 +0200

    correction

--- index.php
+++ index.php
@@ -1,114 +1,85 @@
-<?PHP
-if (isset($_POST['month']) && isset($_POST['year'])) {
-    $selectedMonth = $_POST['month'];
-    $selectedYear = $_POST['year'];
-    $sem = array(6,0,1,2,3,4,5); // Correspondance des jours de la semaine : lundi = 0, dimanche = 6
-    $mois = array('','Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
-    $day = array('lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche');
-    $timestampDay = mktime(0, 0, 0, $selectedMonth, 1, $selectedYear); // Timestamp du premier jour du mois 
-    $currentMonth = date('n',$timestampDay); // Tant que le mois reste celui du départ
-
-    $previousmonth = date('t',mktime(0, 0, 0, $selectedMonth-1, 1, $selectedYear)) - ($sem[date('w',$timestampDay)]-1);
-    $nextmonth = date('j',mktime(0, 0, 0, $selectedMonth+1, 1, $selectedYear));
-    
-    function weeksPerMonth($m,$y){
-        $timestampDay = mktime(1, 1, 1, $m, 1, $y); // cree le timestamp du jour recherché
-        $nday = date('t', $timestampDay); // affiche le nombre de jour du mois, entre 28 et 31
-        $fday = date("N",$timestampDay); // definie le jour du 1er lundi du mois, retourne 1 pour lundi  a 7 pour dimanche
-        $xday = $nday + $fday;  //xday = nb de jour dans le mois + index du 1er ludi
-        if ($xday == 29) {
-            $n = 4; // cas ou fevrier 28jour et le 1er comemence un lundi
-        }else if($xday % 7 != 0){
-            $n = floor($xday/7) + 1;  //cas si $xday % 7jour different de 0
-        }else  {
-            $n = floor($xday/7);   // cas ou $xday = 35
-        }
-        return $n;
-    };
-}
+<?php
+// équivalent d'un if, si on sélectionne février, alors = 2, sinon = mois en cours.
+$selectedMonth = (isset($_POST['months']) ? $_POST['months'] : date('m'));
+$selectedYear = (isset($_POST['years']) ? $_POST['years'] : date('Y'));
+$months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+$numeroDuJour = 1;
+// N = jour de la semaine sous forme de chiffre
+// quand on dit dollar on ne l'écrit pas
+$premierJourDuMois = date('N', mktime(0, 0, 0, $selectedMonth, 1, $selectedYear));
+$nombreDeJourDansLeMois = cal_days_in_month(CAL_GREGORIAN, $selectedMonth, $selectedYear);
+$dernierJourDuMois = date('N', mktime(0, 0, 0, $selectedMonth, $nombreDeJourDansLeMois, $selectedYear));
+$nombreDeCaseAvant = $premierJourDuMois - 1;
+$nombreDeCaseAprès = 7 - $dernierJourDuMois;
+//deux jours avant et deux jours après + 31 = 35
+$nombreDeCaseDuCalendrier = $nombreDeCaseAvant + $nombreDeJourDansLeMois + $nombreDeCaseAprès;
 ?>
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>patie 9 exercice 9</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Calendrier</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
 </head>
 <body>
-    <form action="index.php" method="POST">
-        <select name="month" id="month-select">
-            <option value="1" <?php if (isset($_POST['month']) && $_POST['month'] == '1') { echo ' selected';}?> >janvier</option>
-            <option value="2" <?php if (isset($_POST['month']) && $_POST['month'] == '2') { echo ' selected';}?> >fevrier</option>
-            <option value="3" <?php if (isset($_POST['month']) && $_POST['month'] == '3') { echo ' selected';}?>>mars</option>
-            <option value="4" <?php if (isset($_POST['month']) && $_POST['month'] == '4') { echo ' selected';}?>>avril</option>
-            <option value="5" <?php if (isset($_POST['month']) && $_POST['month'] == '5') { echo ' selected';}?>>mai</option>
-            <option value="6" <?php if (isset($_POST['month']) && $_POST['month'] == '6') { echo ' selected';}?>>juin</option>
-            <option value="7" <?php if (isset($_POST['month']) && $_POST['month'] == '7') { echo ' selected';}?>>juillet</option>
-            <option value="8" <?php if (isset($_POST['month']) && $_POST['month'] == '8') { echo ' selected';}?>>aout</option>
-            <option value="9" <?php if (isset($_POST['month']) && $_POST['month'] == '9') { echo ' selected';}?>>septembre</option>
-            <option value="10" <?php if (isset($_POST['month']) && $_POST['month'] == '10') { echo ' selected';}?>>octobre</option>
-            <option value="11" <?php if (isset($_POST['month']) && $_POST['month'] == '11') { echo ' selected';}?>>novembre</option>
-            <option value="12" <?php if (isset($_POST['month']) && $_POST['month'] == '12') { echo ' selected';}?>>decembre</option>
+    <form action="tp.php" method="POST">
+        <div class="form-group">
+            <label for="months">Mois</label>    
+        <select name="months" id="months">
+            <?php foreach($months as $indexMonth => $month){ ?>
+                <option value="<?= $indexMonth + 1 ?>" <?= $selectedMonth == $indexMonth + 1 ? 'selected' : '' ?> ><?= $month ?></option>
+            <?php } ?>
         </select>
-        <select name="year" id="mois-select">
-            <option value="2000" <?php if (isset($_POST['year']) && $_POST['year'] == '2000') { echo ' selected';}?>>2000</option>
-            <option value="2001" <?php if (isset($_POST['year']) && $_POST['year'] == '2001') { echo ' selected';}?>>2001</option>
-            <option value="2002" <?php if (isset($_POST['year']) && $_POST['year'] == '2002') { echo ' selected';}?>>2002</option>
-            <option value="2003" <?php if (isset($_POST['year']) && $_POST['year'] == '2003') { echo ' selected';}?>>2003</option>
-            <option value="2004" <?php if (isset($_POST['year']) && $_POST['year'] == '2004') { echo ' selected';}?>>2004</option>
-            <option value="2005" <?php if (isset($_POST['year']) && $_POST['year'] == '2005') { echo ' selected';}?>>2005</option>
-            <option value="2006" <?php if (isset($_POST['year']) && $_POST['year'] == '2006') { echo ' selected';}?>>2006</option>
-            <option value="2007" <?php if (isset($_POST['year']) && $_POST['year'] == '2007') { echo ' selected';}?>>2007</option>
-            <option value="2008" <?php if (isset($_POST['year']) && $_POST['year'] == '2008') { echo ' selected';}?>>2008</option>
-            <option value="2009" <?php if (isset($_POST['year']) && $_POST['year'] == '2009') { echo ' selected';}?>>2009</option>
-            <option value="2010" <?php if (isset($_POST['year']) && $_POST['year'] == '2010') { echo ' selected';}?>>2010</option>
-            <option value="2011" <?php if (isset($_POST['year']) && $_POST['year'] == '2011') { echo ' selected';}?>>2011</option>
-            <option value="2012" <?php if (isset($_POST['year']) && $_POST['year'] == '2012') { echo ' selected';}?>>2012</option>
-        </select>
-        <input type="submit" value="Validation">
-        </form>
-        <?php if (isset($_POST['month']) && isset($_POST['year'])) {?>
-            <table class=" table<?php echo weeksPerMonth($_POST['month'], $_POST['year']) ?>weeks">
-                <tr class="tableHeader">
-                    <th colspan="7"><?php echo $mois[$_POST['month']] . ' ' . $_POST['year']?></th>
-                </tr>
-                <tr>
-                    <?php for($i = 0; $i <= 6; $i++){ ?>
-                        <th><?= $day[$i] ?></th>
-                    <?php } ?>
-                </tr>
+        </div>
+        <div class="form-group">
+           <label for="years">Année</label>
+        <select name="years" id="years">
+            <option value="0" selected disabled>Veuillez sélectionner une année</option>
+                <?php //Dans la balise option, on rajoute la valeur dans value et entre les balises
+                    for($year = 1900; $year <= 2100; $year++){ ?>
+                    <option value="<?= $year ?>" <?= $selectedYear == $year ? 'selected' : '' ?>><?= $year ?></option>
+               <?php } ?>
+        </select> 
+        </div>
+            <div class="form-group">
+                <input type="submit" value="Valider" class="btn btn-success" />
+            </div>
+    </form>
+    <table class="table table-striped">
+        <thead>
+            <th>Lundi</th>
+            <th>Mardi</th>
+            <th>Mercredi</th>
+            <th>Jeudi</th>
+            <th>Vendredi</th>
+            <th>Samedi</th>
+            <th>Dimanche</th>
+        </thead>
+        <tbody>
+            <tr>
                 <?php
-                for ($l = 0 ; $l < weeksPerMonth($selectedMonth, $selectedYear) ; $l++){ // calendrier s'adapte au nombre de semaine?>
-                    <tr>
-                        <?php for ($i = 0 ; $i < 7 ; $i++) { // boucle pour afficher 7 td
-                            $currentDay = $sem[date('w',$timestampDay)]; // Jour de la semaine à traiter
-
-                            
-                            if (($currentDay == $i) && $timestampDay < (mktime(0, 0, 0, $selectedMonth, 1, $selectedYear) + (date('t',mktime(0, 0, 0, $selectedMonth, 1, $selectedYear)) * 86400))  && ($currentMonth == $selectedMonth)) { // Si le jours de semaine et le mois correspondent ?>
-                                <td> <?= date('j',$timestampDay) // Affiche le jour du mois?> </td> 
-                                <?php 
-                                $timestampDay += 86400; // timeStamp = au jour suivant
-                            }else { ?>
-                                    <!-- &nbsp; -->
-                                    <?php if ($previousmonth <= date('t',mktime(12, 0, 0, $selectedMonth-1, 1 ,$selectedYear))) { ?>
-                                        <td class="gray">
-                                            <?= $previousmonth; ?>
-                                        </td>
-                                        <?php $previousmonth += 1; 
-                                    }else { ?>
-                                        <td class="gray">
-                                            <?= $nextmonth; ?>
-                                        </td>
-                                        <?php $nextmonth += 1; ?>
-                                    <?php } //fin else nexmonth ?>
-                                </td>
-                            <?php } //fin du else
-                        } //fin boucle affiche les 7 jours?>
-                    </tr>
-                <?php } //fin boucle nombre de semaine ?>
-            </table>
-        <?php } // fin du If isset?>
+                // On utilise une boucle pour générer le bon nombre de cases(jour avant le mois, nombre de jour dans le mois, jour après le mois)
+                 for ($case = 1; $case <= $nombreDeCaseDuCalendrier; $case++) { ?>
+                    <td>
+                    <?php
+                    //on vérifie que la case qui est en train d'être créée, correspond au numéro du premier jour.
+                    //on arrête l'affichage du numero du jour quand il atteint la fin du mois.
+                    if($case >= $premierJourDuMois && $numeroDuJour <= $nombreDeJourDansLeMois){
+                        echo $numeroDuJour;
+                        $numeroDuJour++;
+                    } ?>
+                    </td>
+                   <?php 
+                    // % = modulo
+                    if ($case % 7 == 0) { ?>
+                        </tr><tr>
+                    <?php
+                            }
+                        }
+                    ?>
+            </tr>
+        </tbody>
+    </table>
 </body>
+
 </html>
